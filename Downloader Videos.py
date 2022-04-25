f3d22a2770a791024042067d795d6b99a14cf08a
DasCode <breinner2002@gmail.com>
2022-04-25 15:49:48 -0500

    Upload file

---
+++ Downloader Videos.py
@@ -0,0 +1,41 @@
+# Libraries
+from time import sleep
+import clipboard
+import pytube
+import keyboard
+import os
+from getpass import getuser
+
+# obtain name user of windows
+user = getuser()
+path = rf'C:\Users\{user}\Downloads'
+
+print("welcome to the downloader music, press ctrl+2 for exit ")
+
+while True:
+
+    if keyboard.is_pressed("ctrl+c"):
+        text = clipboard.paste()
+
+        try:
+            yt = pytube.YouTube(text)
+            video = yt.streams.filter(only_audio=True).get_by_itag(251)
+            print(f"Starting download: {video}")
+            file = video.download(path)
+            base, ext = os.path.splitext(file)
+            new_file = base + '.mp3'
+            os.rename(file, new_file)
+            print(f"Done, {new_file}")
+            print("--------------------")
+
+        except FileExistsError:
+            print("This file is existing")
+            print("--------------------")
+
+        except pytube.exceptions.RegexMatchError:
+            print("Format the link is not valid")
+            sleep(1)
+    if keyboard.is_pressed("ctrl+2"):
+        print("Thanks for use my program")
+        exit()
+
